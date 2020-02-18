6f2d06dff30efd1daf1c5d8f4bf21f06daaeaf64
Андрей Беляков <ttxiom@gmail.com>
2020-02-18 10:48:34 +0500

    Update 01.py

--- part-2/01-files-dir/lection/01.py
+++ part-2/01-files-dir/lection/01.py
@@ -1,9 +1,11 @@
-
-
 # 1101 = 13
 
+
+def bin_to_dec(b):
+	d = 0
+	for i in range(len(b)):
+		d += int(b[i])*2**(len(b)-1-i)
+	return d
+
 b = input('Введите двоичное число ')
-d = 0
-for i in range(len(b)):
-	d += int(b[i])*2**(len(b)-1-i)
-print(d)
+print(bin_to_dec(b))
