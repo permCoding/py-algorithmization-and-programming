9a03195f6cfe5e080f2f1f5863630beec9562523
permCoding <tt@59.ru>
2020-03-02 09:20:18 +0500

    lect-03

---
+++ part-2/01-files-dir/lection/lect-03/01.py
@@ -0,0 +1,37 @@
+import os
+
+path = os.curdir
+# print('-', path)
+# path = os.getcwd()
+# print('-', path)
+
+lst_dir = os.listdir(path)
+# print('\n'.join(lst_dir))
+# for item in lst_dir:
+#     print(item)
+
+# for item in lst_dir:
+#     if os.path.isfile(item): # isdir
+#         print(item)
+
+def check(item):
+    return os.path.isfile(item)
+
+# print('\n'.join(list(filter(lambda item: os.path.isfile(item), lst_dir))))
+
+# print('\n'.join(list(filter(check, lst_dir))))
+
+# def check_ext(item, ext):
+#     if os.path.isfile(item):
+
+
+# for item in lst_dir:
+#     if os.path.isfile(item):
+#         if item.split('.')[-1] == 'txt':
+#             print(item)
+
+for item in lst_dir:
+    if os.path.isfile(item):
+        name, ext = os.path.splitext(item)
+        if ext == '.txt':
+            print(name)
