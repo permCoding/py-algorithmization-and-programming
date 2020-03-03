e654237510709452c45dea6706032de13729a686
permCoding <tt@59.ru>
2020-03-03 10:29:42 +0500

    03.py

---
+++ part-2/01-files-dir/lection/lect-03/03.py
@@ -0,0 +1,41 @@
+import os
+
+def print_dir(path):
+    for item in os.listdir(path):
+        if os.path.isfile(item):
+            name, ext = os.path.splitext(item)
+            if ext == '.txt':
+                print(name)
+
+def print_dir_all(dir, check_ext):
+    for item in os.listdir(dir):
+        new_path = os.path.join(dir, item)
+        if os.path.isfile(new_path):
+            name, ext = os.path.splitext(item)
+            if ext == check_ext:
+                print(name)
+        if os.path.isdir(item):
+            print_dir_all(new_path, check_ext)
+
+
+def get_all_files(dir, check_ext):
+    for item in os.listdir(dir):
+        new_path = os.path.join(dir, item)
+        if os.path.isfile(new_path):
+            name, ext = os.path.splitext(item)
+            if ext == check_ext:
+                list_all_files.append(name)
+        if os.path.isdir(item):
+            get_all_files(new_path, check_ext)
+
+list_all_files = []
+path = os.curdir
+ext = '.ini'
+get_all_files(path, ext)
+
+print('\n'.join(list_all_files))
+
+
+
+
+# global path
