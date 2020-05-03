9497c703a0556511f2858d9ed71cbc03ac0984d3
Андрей Беляков <ttxiom@gmail.com>
2020-05-03 20:23:33 +0500

    06

---
+++ part-2/06-tetris/part-2/human.py
@@ -0,0 +1,57 @@
+import pygame
+
+
+def printing():
+    win.blit(image_bg, (0, 0))  # рисуем фон на окне
+    win.blit(images[img_index], (pos_x, pos_y))  # рисуем человека поверх фона
+    pygame.display.update()  # обновляем окно
+
+pygame.init()
+
+image_bg = pygame.image.load('bg1200.jpg')  # загружаем фон игры
+win_width, win_height = image_bg.get_rect().size  # узнаём размеры фона
+win_size = (win_width, win_height)  # запоминаем размеры
+win = pygame.display.set_mode(win_size)  # делаем окно по размерам фона
+win.blit(image_bg, (0, 0))  # рисуем фон на окне
+
+img_names = [
+    'orange/0.png','orange/1.png','orange/2.png','orange/3.png','orange/4.png'
+]  # имена рисунков шагов человека
+images = []  # список для хранения самих рисунков
+for name in img_names:
+    images.append(pygame.image.load(name).convert_alpha())
+
+img_index = 0  # индекс текущего образа человека
+h_w, h_h = images[img_index].get_rect().size  # размеры рисунка человека
+step = h_w // 3  # размер шага человека
+pos_x = 150  # начальная позиция X человека
+pos_y = win_height - h_h - 135  # начальная позиция Y человека
+win.blit(images[img_index], (pos_x, pos_y))  # рисуем текущий образ человека
+
+pygame.display.update()  # обновляем окно
+
+clock = pygame.time.Clock()  # объект для фиксации промежутков времени
+press_time = 0  # время удержания клавиши нажатой
+int_pressed = 100  # интервал паузы, потом смещение человека в mlsek
+
+while True:  # бесконечный цикл приложения, для обработки событий
+    clock.tick()  # в каждой итерации фиксируем время
+    press_time += clock.get_rawtime()  # накапливаем время удержания
+
+    if press_time > int_pressed:  # если превысили интервал паузы
+        press_time = 0  # сбрасываем для следующего цикла удержания
+        key = pygame.key.get_pressed()  # узнаём какая клавиша нажата
+        if pos_x > h_w and key[pygame.K_LEFT]:
+            pos_x -= step  # движение влево
+            img_index = (img_index - 1) % len(images)
+            printing()
+        if pos_x + step < win_width - h_w and key[pygame.K_RIGHT]:
+            pos_x += step  # движение вправо
+            img_index = (img_index + 1) % len(images)  # 0 1 2 -> 3 => 0 1 2
+            printing()
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:  # если нажали ВЫХОД
+            pygame.quit()
+
+
---
+++ part-2/06-tetris/part-3/bombs.py
@@ -0,0 +1,68 @@
+import pygame
+import random
+
+
+pygame.init()
+
+image_bg = pygame.image.load('bg_nebo.jpg')  # фон
+win_width, win_height = image_bg.get_rect().size  # размеры
+win_size = (win_width, win_height)  # запоминаем размеры
+win = pygame.display.set_mode(win_size)  # устанавливаем размеры окна
+win.blit(image_bg, (0, 0))  # заливаем фон
+
+bomb_names = ['bombs/red.png','bombs/green.png']  # имена рисунков бобмочек
+bomb_images = []
+for item in bomb_names:  # добавляем сами рисункци в список
+    bomb_images.append(
+        pygame.image.load(item).convert_alpha()
+    )
+    # bomb_images.append(
+    #     pygame.transform.scale(
+    #         pygame.image.load(item).convert_alpha(),
+    #         (40,40)  # можно изменить размеры
+    #     )
+    # )
+
+bomb_count = 50  # количество бомбочек
+bomb_list = []  # список бобмочек - как спавн препятствий
+spavn_y = 0  # начальный отступ между бомбочками
+for _ in range(bomb_count):
+    img = random.choice(bomb_images)  # случайный рисунок бомбочки
+    x = random.randint(40, win_width-40)  # случайный отступ по X
+    spavn_y += int(1.0 * random.randint(0, win_height))
+    y = 25 - spavn_y  # отступ по Y с накоплением от предыдущей бомбочки
+    v = random.randint(1, 10)  # случайная скорость падения
+    bomb = [img, x, y, v, True]  # для каждой бомбочки свои параметры
+    bomb_list.append(bomb)  # добавляем бомбу в список
+
+clock = pygame.time.Clock()  # объект для отсчёта времени
+time_fall = 0  # текущее время задержки падения всех бомбочек
+int_fall = 10  # интервал до следующего сдвига mlsek
+
+pygame.display.update()  # отрисовываем фон
+
+run = True
+while run:  # бесконечный цикл приложения, для обработки событий
+
+    clock.tick()  # в каждой итерации фиксируем время
+    time_fall += clock.get_rawtime()  # накапливаем время паузы
+    if time_fall >= int_fall:  # если превысили интервал паузы
+        time_fall = 0  # сбрасываем для следующего цикла паузы
+        for i in range(bomb_count):  # для всех бомб из списка
+            if bomb_list[i][4]:  # если бомба ещё не достигла дна
+                bomb_list[i][2] += bomb_list[i][3]  # смещаем её на шаг
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # если нажали ВЫХОД
+            run = False
+
+    win.blit(image_bg, (0, 0))  # рисуем фон на окне
+    for i in range(bomb_count):  # для всех бомб из списка
+        if bomb_list[i][4]:  # если бомба ещё не достигла дна
+            win.blit(bomb_list[i][0], (bomb_list[i][1], bomb_list[i][2]))  # рисуем бомбу
+            if bomb_list[i][2] > win_height - 216:  # если бомба достигла дна
+                bomb_list[i][4] = False  # отключаем бомбу
+
+    pygame.display.update()  # отрисовываем всё
+
+pygame.quit()
